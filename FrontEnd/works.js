cc9712f02f9680f3992a6600a76e233cde761823
Minyoung <sinkuu@naver.com>
2023-05-18 19:53:02 +0200

    Filtres des travaux 70%

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,33 +1,55 @@
 const worksUrl = "http://localhost:5678/api/works";
 const categoriesUrl = "http://localhost:5678/api/categories";
 const gallery = document.querySelector(".gallery");
+const filtre = document.querySelector(".filtres");
 
 fetch(worksUrl)
   .then((response) => response.json())
-  .then((works) => {
-    for (i = 0; i < works.length; i++) {
-      const worksSource = works[i];
-      const figureElement = document.createElement("figure");
-      const imageElement = document.createElement("img");
-      imageElement.src = worksSource.imageUrl;
-      imageElement.alt = worksSource.title;
-      const figcaptionElement = document.createElement("figcaption");
-      figcaptionElement.innerText = worksSource.title;
+  .then(displayWorks);
 
-      gallery.appendChild(figureElement);
-      figureElement.appendChild(imageElement);
-      figureElement.appendChild(figcaptionElement);
-    }
-  });
+fetch(categoriesUrl)
+  .then((response) => response.json())
+  .then(displayCategories);
 
-const filter = document.querySelectorAll(".filtre-btn");
+function displayWorks(works) {
+  gallery.innerHTML = "";
+  console.log(works);
+  for (i = 0; i < works.length; i++) {
+    const worksSource = works[i];
+    const figureElement = document.createElement("figure");
+    const imageElement = document.createElement("img");
+    imageElement.src = worksSource.imageUrl;
+    imageElement.alt = worksSource.title;
+    const figcaptionElement = document.createElement("figcaption");
+    figcaptionElement.innerText = worksSource.title;
 
-function showWorksFiltered() {
-  fetch(categoriesUrl)
-    .then((response) => response.json())
-    .then((categories) => {
-      console.log(categories);
-    });
+    gallery.appendChild(figureElement);
+    figureElement.appendChild(imageElement);
+    figureElement.appendChild(figcaptionElement);
+  }
 }
 
-filter.forEach((button) => button.addEventListener("click", showWorksFiltered));
+function displayCategories(categories) {
+  filtre.innerHTML = "";
+  const categoryDefault = {
+    id: 0,
+    name: "Tous",
+  };
+  categories.unshift(categoryDefault);
+
+  for (i = 0; i < categories.length; i++) {
+    const categoriesSource = categories[i];
+    const buttonElement = document.createElement("button");
+    buttonElement.innerText = categoriesSource.name;
+    buttonElement.classList.add("filtre-btn");
+
+    // const categorySet = new Set(categories);
+    // const categorySetValues = categorySet.values();
+    // for (const value of categorySetValues) {
+    // }
+
+    filtre.appendChild(buttonElement);
+  }
+}
+
+filtre.addEventListener("click", (name) => displayWorks(name));

bb3665577a475cf9a19b52abfdfc637d77e35cd6
Minyoung <sinkuu@naver.com>
2023-05-23 18:47:13 +0200

    Fini le tri des projets par catégorie

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -3,9 +3,13 @@ const categoriesUrl = "http://localhost:5678/api/categories";
 const gallery = document.querySelector(".gallery");
 const filtre = document.querySelector(".filtres");
 
-fetch(worksUrl)
-  .then((response) => response.json())
-  .then(displayWorks);
+let works;
+async function getWorks() {
+  works = await fetch(worksUrl).then((response) => response.json());
+  displayWorks(works);
+}
+
+getWorks();
 
 fetch(categoriesUrl)
   .then((response) => response.json())
@@ -13,7 +17,7 @@ fetch(categoriesUrl)
 
 function displayWorks(works) {
   gallery.innerHTML = "";
-  console.log(works);
+
   for (i = 0; i < works.length; i++) {
     const worksSource = works[i];
     const figureElement = document.createElement("figure");
@@ -29,6 +33,15 @@ function displayWorks(works) {
   }
 }
 
+function filterWorks(event) {
+  const categoriesId = event.target.id;
+  if (categoriesId == 0) {
+    return displayWorks(works);
+  }
+  const filterDeWorks = works.filter((work) => work.categoryId == categoriesId);
+  displayWorks(filterDeWorks);
+}
+
 function displayCategories(categories) {
   filtre.innerHTML = "";
   const categoryDefault = {
@@ -40,16 +53,12 @@ function displayCategories(categories) {
   for (i = 0; i < categories.length; i++) {
     const categoriesSource = categories[i];
     const buttonElement = document.createElement("button");
+    buttonElement.id = categoriesSource.id;
     buttonElement.innerText = categoriesSource.name;
     buttonElement.classList.add("filtre-btn");
-
-    // const categorySet = new Set(categories);
-    // const categorySetValues = categorySet.values();
-    // for (const value of categorySetValues) {
-    // }
 
     filtre.appendChild(buttonElement);
   }
 }
 
-filtre.addEventListener("click", (name) => displayWorks(name));
+filtre.addEventListener("click", filterWorks);
